2a1034ca5e11cb237213a2ea03d6a33bdcaecdfc
esgaelramos <esgaelramos@gmail.com>
2022-02-07 17:51:32 -0600

    Create a typewriter.js, and the first section of my web (banner) and load the images

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var xsexy = new Vue({
+    el: '#x',
+    // --- DATA --- //
+    data: {
+
+    },
+    // --- COMPUTED --- //
+    computed: {
+
+    },
+    // --- WHATCHING --- //
+    watch: {
+
+    },
+    // --- METHODS --- //
+    methods: {
+
+    },
+})
